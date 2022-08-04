b351c6d6363543d638fbaaf3342688ef9f4b48f1
allen <g01.webapp@gmail.com>
2022-08-04 13:15:30 +0800

    加入專案檔案。

---
+++ UIDemo01/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using WA.Holidays.SDK;
+
+var container = new HolidayContainer();
+var data =await container.未來的非六日假日();
+
+foreach (HolidayContainer.HolidayVM holidayVM in data)
+{
+	Console.WriteLine($"距離 {holidayVM.Name} 還剩 {holidayVM.DiffDays} 天, Date={holidayVM.Date:yyyy/MM/dd}");
+}
+
+Console.ReadKey();
---
+++ WA.Holidays.SDK/Facade.cs
@@ -0,0 +1,87 @@
+﻿
+using Newtonsoft.Json;
+
+namespace WA.Holidays.SDK
+{
+	public class Facade
+	{
+		public async Task<IEnumerable<HolidayFromJson>> GetData()
+		{
+			string url =
+				"https://quality.data.gov.tw/dq_download_json.php?nid=145708&md5_url=1a7506b007086d03afe37570ac7f52e4";
+
+			using var response = await new HttpClient().GetAsync(url);
+			response.EnsureSuccessStatusCode();
+
+			var contentString = await response.Content.ReadAsStringAsync();
+
+			var holidays = JsonConvert.DeserializeObject<List<HolidayFromJson>>(contentString);
+
+			return holidays;
+		}
+	}
+
+	public class HolidayFromJson
+	{
+		public DateTime Date { get; set; }
+		public string Name { get; set; }
+		public string IsHoliday { get; set; }
+		public string HolidayCategory { get; set; }
+		public string Description { get; set; }
+	}
+
+	public class JsonHolidaysAdapter
+	{
+		public async Task<IEnumerable<Holiday>> GetHolidays()
+		{
+			var facade = new Facade();
+			var jsonData = await facade.GetData();
+
+			return jsonData.Select(x => x.ToHoliday());
+		}
+	}
+	public class Holiday
+	{
+		public DateTime Date { get; set; }
+		public string Name { get; set; }
+		public bool IsHoliday { get; set; }
+		public string HolidayCategory { get; set; }
+		public bool IsSaturdayOrSunday => HolidayCategory == "星期六、星期日";
+		public override string ToString()
+		{
+			return $"{Date.ToString("yyyy/MM/dd")}, Name={Name}, IsHoliday={IsHoliday}, 是六日={IsSaturdayOrSunday}, category={HolidayCategory}";
+		}
+	}
+
+	public static class HolidayFromJsonExts
+	{
+		public static Holiday ToHoliday(this HolidayFromJson source)
+		{
+			return new Holiday
+			{
+				Date = source.Date,
+				Name = source.Name,
+				IsHoliday = source.IsHoliday == "是" ? true : false,
+				HolidayCategory = source.HolidayCategory,
+			};
+		}
+	}
+
+	public class HolidayContainer
+	{
+		public async Task<IEnumerable<HolidayVM>> 未來的非六日假日()
+		{
+			var allHolidays= await new JsonHolidaysAdapter().GetHolidays();
+			return allHolidays
+				.Where(h => h.IsSaturdayOrSunday == false && h.Date >= DateTime.Today)
+				.Select(h => new HolidayVM { Date = h.Date, Name = h.Name});
+		}
+
+		public class HolidayVM
+		{
+			public DateTime Date { get; set; }
+			public string Name { get; set; }
+			public int DiffDays => (Date - DateTime.Today).Days;
+		}
+	}
+}
